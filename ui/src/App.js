fa9cd1ade11985ea11518b191b4723f282e6de6c
Aspen James <aspenjames@tqca.org>
2020-09-10 21:37:44 -0700

    add hot reload

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 
 function App(props) {
     return (
-        <h1>Hello</h1>
+        <h1>Hello, earthlings</h1>
     );
 }
 
-export default App;
+export default hot(module)(App);
