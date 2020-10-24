000fc0c8ea62dee2c6fcb1f351db291b9374f6fd
Aspen James <aspenjames@tqca.org>
2020-10-24 15:48:22 -0700

    setup application to serve fe build

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/aspenjames/aspenjames.dev
 go 1.15
 
 require (
-	github.com/gin-gonic/contrib v0.0.0-20200810162008-6dee08bf958e // indirect
+	github.com/gin-gonic/contrib v0.0.0-20200810162008-6dee08bf958e
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/assert/v2 v2.0.1
 )
--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"github.com/aspenjames/aspenjames.dev/api"
+	"github.com/gin-gonic/contrib/static"
 )
 
 func main() {
+	// Set up api router
 	router := api.SetupRouter()
-	router.Run(":4000") // run on port 4000
+
+	// Serve frontend files
+	router.Use(static.Serve("/", static.LocalFile("./ui/public", true)))
+	router.Use(static.Serve("/dist", static.LocalFile("./ui/dist", true)))
+
+	// run on port 4000
+	router.Run(":4000")
 }
