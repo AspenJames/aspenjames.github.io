59cc32b99646b18c9d3d44c6609224235039a782
Aspen James <aspenjames@tqca.org>
2021-06-11 20:33:22 -0700

    Add to about, link from home

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,10 +16,12 @@ function About() {
         I'm a full stack engineer; I build web-based software products with a
         focus on developer tooling and automation. I am a trans/non-binary queer
         person, and I use they/them pronouns. I live and work on the <Link
-        href="https://www.duwamishtribe.org/history">unceded territory of the
-        Dx&#695;d&#601;w&#660;ab&scaron;</Link>, and it's important to me that I
+          href="https://www.duwamishtribe.org/history">unceded territory of the
+          Dx&#695;d&#601;w&#660;ab&scaron;</Link>, and it's important to me that I
         acknowledge this and <Link href="https://www.realrentduwamish.org/">pay
-        rent</Link> to the original people of this land.
+          rent</Link> to the original people of this land. When I'm not working, I
+        enjoy making and drinking coffee, film and Polaroid photography, and
+        generally being around trees and nature.
       </TextRight>
     </ContainerRight>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
+import { Link } from '@material-ui/core';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import ContainerRight from '../components/ContainerRight';
 import TextRight from '../components/TextRight';
@@ -15,6 +17,9 @@ function Home() {
       </TextRight>
       <TextRight variant="h3" color="textSecondary">
         i'm a developer.
+      </TextRight>
+      <TextRight variant="h3" color="textSecondary">
+        <Link component={NavLink} to="/about">want to know more?</Link>
       </TextRight>
     </ContainerRight>
   );
