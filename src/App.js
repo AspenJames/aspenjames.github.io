3664d9454924288317f2bf56d9779838225b71fd
Aspen James <aspenjames@tqca.org>
2021-06-11 21:06:51 -0700

    use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from "react";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import clsx from "clsx";
 
 import {
