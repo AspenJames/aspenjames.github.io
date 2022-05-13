f262240e4283e1f9761a1fc93d2cc15b169afb67
Aspen James <hello@aspenjames.dev>
2022-05-13 10:55:25 -0700

    Remove deprecated function, linting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useMemo, useState } from "react";
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from "react-router-dom";
 import clsx from "clsx";
 
 import {
-  createMuiTheme,
+  createTheme,
   CssBaseline,
   makeStyles,
   ThemeProvider,
@@ -11,34 +11,34 @@ import {
 } from "@material-ui/core";
 
 import NavBar from "./components/NavBar";
-import Routes from './Routes';
+import Routes from "./Routes";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    height: '90vh',
+    display: "flex",
+    height: "90vh",
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: -drawerWidth,
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -47,62 +47,59 @@ const useStyles = makeStyles(theme => ({
 }));
 
 function App() {
-  let darkModeInitial = localStorage.getItem('darkMode');
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  let darkModeInitial = localStorage.getItem("darkMode");
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   if (darkModeInitial === null) {
     darkModeInitial = prefersDarkMode;
-    localStorage.setItem('darkMode', darkModeInitial)
+    localStorage.setItem("darkMode", darkModeInitial);
   } else {
-    darkModeInitial = darkModeInitial !== 'false';
+    darkModeInitial = darkModeInitial !== "false";
   }
   const [isDarkMode, setDarkMode] = useState(darkModeInitial);
-  const theme = useMemo(
-    () => {
-      const darkThemeColors = {
-        primary: {
-          light: '#72FFFF',
-          main: '#00FFFF',
-          dark: '#00CBCC',
-          contrastText: '#000000',
-        },
-        secondary: {
-          light: '#FFD7FF',
-          main: '#FFA4FF',
-          dark: '#CB73CC',
-          contrastText: '#000000',
-        },
-      }
-      const lightThemeColors = {
-        primary: {
-          light: '#64FFFF',
-          main: '#00CCCC',
-          dark: '#009A9B',
-          contrastText: '#000000',
-        },
-        secondary: {
-          light: '#FFC1E3',
-          main: '#F48FB1',
-          dark: '#BF5F82',
-          contrastText: '#FFFFFF',
-        },
-      }
-      if (isDarkMode) {
-        return createMuiTheme({
-          palette: {
-            type: 'dark',
-            ...darkThemeColors,
-          }
-        })
-      }
-      return createMuiTheme({
+  const theme = useMemo(() => {
+    const darkThemeColors = {
+      primary: {
+        light: "#72FFFF",
+        main: "#00FFFF",
+        dark: "#00CBCC",
+        contrastText: "#000000",
+      },
+      secondary: {
+        light: "#FFD7FF",
+        main: "#FFA4FF",
+        dark: "#CB73CC",
+        contrastText: "#000000",
+      },
+    };
+    const lightThemeColors = {
+      primary: {
+        light: "#64FFFF",
+        main: "#00CCCC",
+        dark: "#009A9B",
+        contrastText: "#000000",
+      },
+      secondary: {
+        light: "#FFC1E3",
+        main: "#F48FB1",
+        dark: "#BF5F82",
+        contrastText: "#FFFFFF",
+      },
+    };
+    if (isDarkMode) {
+      return createTheme({
         palette: {
-          type: 'light',
-          ...lightThemeColors,
-        }
-      })
-    },
-    [isDarkMode]
-  )
+          type: "dark",
+          ...darkThemeColors,
+        },
+      });
+    }
+    return createTheme({
+      palette: {
+        type: "light",
+        ...lightThemeColors,
+      },
+    });
+  }, [isDarkMode]);
   theme.drawerWidth = drawerWidth;
 
   const classes = useStyles();
@@ -125,7 +122,6 @@ function App() {
             <div className={classes.drawerHeader} />
 
             <Routes />
-
           </main>
         </Router>
       </div>
